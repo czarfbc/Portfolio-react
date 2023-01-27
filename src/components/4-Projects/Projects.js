34c8f2c94813ac8bc00a574d967cd1dd88525b8a
czarfbc <cezarfbc@gmail.com>
2023-01-27 16:34:59 -0300

    projetos

--- src/components/4-Projects/Projects.js
+++ src/components/4-Projects/Projects.js
@@ -15,24 +15,6 @@ function Projects() {
                 <a className='projects_btn_a btn_a_2' href='/'>Preview</a>
               </div>
             </div>
-            <div className='projects_box_single'>
-              <h4>Linguagem predominante</h4>
-              <img className='projects_img' src={css3} alt=''/>
-              <h5>Lorem Ipsum</h5>
-              <div className='projects_btn'>
-                <a className='projects_btn_a btn_a_1' href='/'>Github</a>
-                <a className='projects_btn_a btn_a_2' href='/'>Preview</a>
-              </div>
-            </div>
-            <div className='projects_box_single'>
-              <h4>Linguagem predominante</h4>
-              <img className='projects_img' src={css3} alt=''/>
-              <h5>Lorem Ipsum</h5>
-              <div className='projects_btn'>
-                <a className='projects_btn_a btn_a_1' href='/'>Github</a>
-                <a className='projects_btn_a btn_a_2' href='/'>Preview</a>
-              </div>
-            </div>
         </div> 
       </div>
     );
