0879ddda6e2d170e1b3015073e9c732bb9750aca
czarfbc <cezarfbc@gmail.com>
2023-03-13 13:54:30 -0300

    excluir comentario

--- src/components/4-Projects/Projects.js
+++ src/components/4-Projects/Projects.js
@@ -28,7 +28,7 @@ function Projects() {
             break;
 
           case "SCSS":
-            iconProject = assets.scss_image
+            iconProject = assets.sass_image
             break;
 
           default:
@@ -44,7 +44,7 @@ function Projects() {
               <h5>{repo.name}</h5>
               <div className='projects_btn'>
                 <a className='projects_btn_a btn_a_1' href={repo.html_url} rel='noreferrer' target='_blank'>Github</a>
-                <a className='projects_btn_a btn_a_2' href='https://czar-portifolio.netlify.app/' rel='noreferrer' target='_blank'>Preview</a>
+                <a className='projects_btn_a btn_a_2' href={repo.html_url} rel='noreferrer' target='_blank'>Preview</a>
               </div>
             </div>
             
@@ -52,67 +52,6 @@ function Projects() {
         }
 
       })}
-{/* 
-        <div className='projects_box_single'>
-          <h4>Linguagem predominante</h4>
-          <img className='projects_img' src={assets.css_image} alt=''/>
-          <h5>Portfólio_v1.0</h5>
-          <div className='projects_btn'>
-            <a className='projects_btn_a btn_a_1' href='https://github.com/czarfbc/Portfolio' rel='noreferrer' target='_blank'>Github</a>
-            <a className='projects_btn_a btn_a_2' href='https://czar-portifolio.netlify.app/' rel='noreferrer' target='_blank'>Preview</a>
-          </div>
-        </div>
-
-        <div className='projects_box_single'>
-          <h4>Linguagem predominante</h4>
-          <img className='projects_img' src={assets.css_image} alt=''/>
-          <h5>Pokedex_v1.0</h5>
-          <div className='projects_btn'>
-            <a className='projects_btn_a btn_a_1' href='https://github.com/czarfbc/Pokedex' rel='noreferrer' target='_blank'>Github</a>
-            <a className='projects_btn_a btn_a_2' href='https://czar-pokedex.netlify.app/' rel='noreferrer' target='_blank'>Preview</a>
-          </div>
-        </div>
-
-        <div className='projects_box_single'>
-          <h4>Linguagem predominante</h4>
-          <img className='projects_img' src={assets.js_image} alt=''/>
-          <h5>Previsao_Tempo_v0.1</h5>
-          <div className='projects_btn'>
-            <a className='projects_btn_a btn_a_1' href='https://github.com/czarfbc/Previsao-tempo' rel='noreferrer' target='_blank'>Github</a>
-            <a className='projects_btn_a btn_a_2' href='https://github.com/czarfbc/Previsao-tempo' rel='noreferrer' target='_blank'>Preview</a>
-          </div>
-        </div> */}
-
-        <div className='projects_box_single'>
-          <h4>Linguagem predominante</h4>
-          <img className='projects_img' src={assets.sass_image} alt=''/>
-          <h5>Portal-Noticias</h5>
-          <div className='projects_btn'>
-            <a className='projects_btn_a btn_a_1' href='https://github.com/czarfbc/Portal-Noticias' rel='noreferrer' target='_blank'>Github</a>
-            <a className='projects_btn_a btn_a_2' href='https://github.com/czarfbc/Portal-Noticias' rel='noreferrer' target='_blank'>Preview</a>
-          </div>
-        </div>
-
-        <div className='projects_box_single'>
-          <h4>Linguagem predominante</h4>
-          <img className='projects_img' src={assets.js_image} alt=''/>
-          <h5>To-do-list</h5>
-          <div className='projects_btn'>
-            <a className='projects_btn_a btn_a_1' href='https://github.com/czarfbc/To-do-list-nodejs' rel='noreferrer' target='_blank'>Github</a>
-            <a className='projects_btn_a btn_a_2' href='https://github.com/czarfbc/To-do-list-nodejs' rel='noreferrer' target='_blank'>Preview</a>
-          </div>
-        </div>
-
-        <div className='projects_box_single'>
-          <h4>Linguagem predominante</h4>
-          <img className='projects_img' src={assets.js_image} alt=''/>
-          <h5>Pedra-Papel-Tesoura</h5>
-          <div className='projects_btn'>
-            <a className='projects_btn_a btn_a_1' href='https://github.com/czarfbc/Pedra--Papel--Tesoura' rel='noreferrer' target='_blank'>Github</a>
-            <a className='projects_btn_a btn_a_2' href='https://github.com/czarfbc/Pedra--Papel--Tesoura' rel='noreferrer' target='_blank'>Preview</a>
-          </div>
-        </div>
-
       </div> 
     </div>
   );
